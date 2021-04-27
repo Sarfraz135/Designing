03c2b2559e032a59310d0dad73ea3b8286006189
Sarfraz Aslam <82870539+Sarfraz135@users.noreply.github.com>
2021-04-27 10:09:36 -0700

    Add files via upload

---
+++ #1 HTML CSS bootstrape/#9 Class/app.js
@@ -0,0 +1,58 @@
+alert("Hello World");
+
+
+// var a = 5;
+// var b =10;
+// var c=a+b;
+// alert(c);
+
+
+var a = 5;
+var b =10;
+var c=a+b;
+console.log(c);
+
+
+var a = 5;
+var b ="Ali";
+var c=a+b;
+alert(c);
+
+var a = "Sarfraz";
+var b =" Aslam";
+var c=a+b;
+alert(c);
+
+
+// concetenation is always for + sign 
+
+
+
+var a = "20";
+var b ="10";
+var c=a+b;
+alert(c);
+
+
+var a = "20";
+var b ="2";
+var c=a*b;
+alert(c);
+
+
+var a = "20";
+var b ="2";
+var c=(a*b)-b / a;
+alert(c);
+
+var a = 5;
+var b = a++;
+alert(b);
+alert(a);
+
+
+var x=10;
+var y = x++ + ++x - x + x++ - ++x + x;
+
+//solution 10 +   12  - 12 + 12 - 14 + 14 = 22
+var z = alert(y);
